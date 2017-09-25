2a699a35619c055eea50b7d431c449b3ab666582
Daniel Bicsak <bicsak.daniel@gmail.com>
2017-09-25 14:34:22 +0200

    BaseEntity updated

--- src/main/java/entity/BaseEntity.java
+++ src/main/java/entity/BaseEntity.java
@@ -2,9 +2,16 @@ package entity;
 
 import lombok.Data;
 
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 
 @Data
 @MappedSuperclass
 public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Version
+    private int version;
 }
